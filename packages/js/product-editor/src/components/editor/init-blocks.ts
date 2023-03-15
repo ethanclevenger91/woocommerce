3d2b716d686a11f089c4907b6606381fb39d3553
Maikel David Pérez Gómez <mdperez86@gmail.com>
2023-03-15 10:41:47 -0300

    Add summary block

--- packages/js/product-editor/src/components/editor/init-blocks.ts
+++ packages/js/product-editor/src/components/editor/init-blocks.ts
@@ -7,6 +7,7 @@ import { registerCoreBlocks } from '@wordpress/block-library';
  * Internal dependencies
  */
 import { init as initName } from '../details-name-block';
+import { init as initSummary } from '../details-summary-block';
 import { init as initSection } from '../section';
 import { init as initTab } from '../tab';
 import { init as initPricing } from '../pricing-block';
@@ -14,6 +15,7 @@ import { init as initPricing } from '../pricing-block';
 export const initBlocks = () => {
 	registerCoreBlocks();
 	initName();
+	initSummary();
 	initSection();
 	initTab();
 	initPricing();
