c13de5cadba10fce02b9b2eeae53f77b36e5c929
Lourens Schep <lourensschep@gmail.com>
2023-03-17 15:26:32 +0100

    Add initial category block

--- packages/js/product-editor/src/components/editor/init-blocks.ts
+++ packages/js/product-editor/src/components/editor/init-blocks.ts
@@ -7,12 +7,14 @@ import { registerCoreBlocks } from '@wordpress/block-library';
  * Internal dependencies
  */
 import { init as initName } from '../details-name-block';
+import { init as initCategory } from '../category-block';
 import { init as initSection } from '../section';
 import { init as initTab } from '../tab';
 import { init as initPricing } from '../pricing-block';
 
 export const initBlocks = () => {
 	registerCoreBlocks();
+	initCategory();
 	initName();
 	initSection();
 	initTab();
